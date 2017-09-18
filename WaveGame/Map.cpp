853b27c3d43e3d5e43834f8b3284d0905410e0d5
daniel crutchley-bentham <sherbet.gray54@googlemail.com>
2017-09-18 14:18:46 +0100

    Added documentation for the Map class.

--- WaveGame/Map.cpp
+++ WaveGame/Map.cpp
@@ -15,12 +15,20 @@ Map::~Map()
 {
 }
 
+/// <summary>
+/// Adds a prop to the world.
+/// </summary>
+/// <param name = "Prop">The prop to add.</param>
 void Map::AddProp(shared_ptr<Prop> Prop)
 {
 	this->MapProps.push_back(Prop);
 }
 
-void Map::Draw(RenderWindow * Window)
+/// <summary>
+/// Renders all the props, and the background, to the screen.
+/// </summary>
+/// <param name = "Window">The rende target.</param>
+void Map::Draw(RenderWindow* Window)
 {
 	this->Background.Draw(Window);
 
@@ -28,6 +36,11 @@ void Map::Draw(RenderWindow * Window)
 		P->Draw(Window);
 }
 
+/// <summary>
+/// Returns all the props in the world that has a certain tag.
+/// </summary>
+/// <param name = "Tag">The tag to find props with.</param>
+/// <returns>The props with that tag.</returns>
 vector<shared_ptr<Prop>> Map::GetPropsWithTag(string Tag)
 {
 	vector<shared_ptr<Prop>> Props;
