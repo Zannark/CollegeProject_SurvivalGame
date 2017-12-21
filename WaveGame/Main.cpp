127b7e12d56459fc6151df5c2a489c3203a2d1d7
Zannark <sherbet.gray54@googlemail.com>
2017-12-21 17:09:20 +0000

    Tested jazz

--- WaveGame/Main.cpp
+++ WaveGame/Main.cpp
@@ -5,6 +5,7 @@
 #include "GameTime.h"
 #include "Enemy.h"
 #include <SFML\Graphics.hpp>
+#include <array>
 
 using namespace sf;
 
@@ -18,9 +19,18 @@ int main(int argc, char** argv)
 	
 	shared_ptr<Engine::GamePlay::Player> P = make_shared<Engine::GamePlay::Player>();
 	Engine::Core::Map M = Engine::Core::MapLoader::Load("Test.xml", Window);
-	Engine::GamePlay::Enemy En = Engine::GamePlay::Enemy(Vector2f(0, 0), Window, P);
+	//Engine::GamePlay::Enemy En = Engine::GamePlay::Enemy(Vector2f(0, 0), Window, P);
 
 	Engine::Core::CreateNavigationMesh(Window, *P, M);
+
+	Enemy e1 = Enemy(Vector2f(0, 0), Window, P);
+	Enemy e2 = Enemy(Vector2f(32, 0), Window, P);
+	Enemy e3 = Enemy(Vector2f(64, 0), Window, P);
+	Enemy e4 = Enemy(Vector2f(96, 0), Window, P);
+	Enemy e5 = Enemy(Vector2f(128, 0), Window, P);
+	Enemy e6 = Enemy(Vector2f(160, 0), Window, P);
+	Enemy e7 = Enemy(Vector2f(192, 0), Window, P);
+	Enemy e8 = Enemy(Vector2f(200, 0), Window, P);
 
 	while (Window->isOpen())
 	{
@@ -33,14 +43,33 @@ int main(int argc, char** argv)
 		}
 
 		P->Update(Window, M, GameTime::DeltaTime());
-		En.Update(Window, M, GameTime::DeltaTime());
+		//En.Update(Window, M, GameTime::DeltaTime());
+		// Why does it randomly corrupt itself
+
+		e1.Update(Window, M, GameTime::DeltaTime());
+		e2.Update(Window, M, GameTime::DeltaTime());
+		e3.Update(Window, M, GameTime::DeltaTime());
+		e4.Update(Window, M, GameTime::DeltaTime());
+		e5.Update(Window, M, GameTime::DeltaTime());
+		e6.Update(Window, M, GameTime::DeltaTime());
+		e7.Update(Window, M, GameTime::DeltaTime());
+		e8.Update(Window, M, GameTime::DeltaTime());
 		
 		Window->clear(Color::Cyan);
 		
 		M.DrawBackground(Window);
 		P->Draw(Window);
 		M.DrawProps(Window);
-		En.Draw(Window);
+		//En.Draw(Window);
+
+		e1.Draw(Window);
+		e2.Draw(Window);
+		e3.Draw(Window);
+		e4.Draw(Window);
+		e5.Draw(Window);
+		e6.Draw(Window);
+		e7.Draw(Window);
+		e8.Draw(Window);
 
 		Window->display();
 		GameTime::Update();
